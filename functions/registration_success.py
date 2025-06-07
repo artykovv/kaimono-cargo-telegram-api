2dce6530e58eff722212259d8463c37c0a15d334
Jalil Artykov <artykovjalil@gmail.com>
2025-06-07 16:12:05 +0600

    add api key for reg success

--- functions/registration_success.py
+++ functions/registration_success.py
@@ -1,13 +1,17 @@
 
 import httpx
 from config.config import TELEGRAM_API
+from config.config import API_KEY
 
 async def send_registration_success_message(user):
     async with httpx.AsyncClient() as client:
         try:
             url = f"{TELEGRAM_API}/registration/success/{user.telegram_chat_id}"
-
-            response = await client.post(url=url)
+            headers = {
+                "X-API-Key": API_KEY,
+                "Accept": "application/json"
+            }
+            response = await client.post(url=url, headers=headers)
             response.raise_for_status()  # Проверка на ошибки HTTP
             return response
         except httpx.HTTPStatusError as e:
